5b06ca5a04e6f54523de5ae404aa93b8a68b20c0
henriquemantovan <henrique.mantovan@usp.br>
2024-10-07 19:29:02 -0300

    troquei switch por if

--- testar.ino
+++ testar.ino
@@ -12,7 +12,6 @@ volatile char comando;
 
 Carrinho meuCarrinho(pin1, pin2, pin3, pin4, LED);  
 
-
 void setup() {
     inicializarSistema();
 }
@@ -24,45 +23,40 @@ void movimentar(void * pvParameters) {
         //NAO TESTADO
         meuCarrinho.nucleosControle(PARAMETROS); // ta na biblioteca o define//PARAMETROS = SerialBT, comando, xMutex, sinal_entre_nucleos
 
-        switch (comando) {
-            case 'F': { //FRENTE
-                meuCarrinho.motor_Esq(1);
-                meuCarrinho.motor_Dir(1);
-                break;
-            }
-            case 'S': {//PARA
-                meuCarrinho.motor_Esq(0);
-                meuCarrinho.motor_Dir(0);
-                break;
-            }
-            case 'B': {//RÉ
-                meuCarrinho.motor_Esq(-1);
-                meuCarrinho.motor_Dir(-1);
-                break;
-            }
-            case 'R': case 'E': {//FRETE DIREITA
-                meuCarrinho.motor_Esq(0);
-                meuCarrinho.motor_Dir(1);
-                break;
-            }
-            case 'Q': case 'L': {//FRENTE ESQUERDA
-                meuCarrinho.motor_Esq(1);
-                meuCarrinho.motor_Dir(0);
-                break;
-            }
-            case 'C': {//RÉ DIREITA
-                meuCarrinho.motor_Esq(0);
-                meuCarrinho.motor_Dir(-1);
-                break;
-            }
-            case 'Z': {//RÉ ESQUERDA
-                meuCarrinho.motor_Esq(-1);
-                meuCarrinho.motor_Dir(0);
-                break;
-            }
-            default:
-                break;
-        }
+        if (comando == 'F') { //FRENTE
+            meuCarrinho.motor_Esq(1);
+            meuCarrinho.motor_Dir(1);
+        } 
+
+        else if (comando == 'S') { //PARA
+            meuCarrinho.motor_Esq(0);
+            meuCarrinho.motor_Dir(0);
+        } 
+
+        else if (comando == 'B') { //RÉ
+            meuCarrinho.motor_Esq(-1);
+            meuCarrinho.motor_Dir(-1);
+        } 
+
+        else if (comando == 'R' || comando == 'E') { //FRETE DIREITA
+            meuCarrinho.motor_Esq(0);
+            meuCarrinho.motor_Dir(1);
+        } 
+
+        else if (comando == 'Q' || comando == 'L') { //FRENTE ESQUERDA
+            meuCarrinho.motor_Esq(1);
+            meuCarrinho.motor_Dir(0);
+        } 
+
+        else if (comando == 'C') { //RÉ DIREITA
+            meuCarrinho.motor_Esq(0);
+            meuCarrinho.motor_Dir(-1);
+        } 
+
+        else if (comando == 'Z') {//RÉ ESQUERDA
+            meuCarrinho.motor_Esq(-1);
+            meuCarrinho.motor_Dir(0);
+        }  
         vTaskDelay(100);
     }
 }
