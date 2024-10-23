1ad8f5f3930bd4d24b9e5f0734daa302b7e69b16
henriquemantovan <henrique.mantovan@usp.br>
2024-10-23 01:15:11 -0300

    p

--- main.ino
+++ main.ino
@@ -3,32 +3,28 @@
 
 #define pin1 17   
 #define pin2 18 
-#define pin3 19 //NAO TA INDO
+#define pin3 19 
 #define pin4 23 
 #define LED 4
-#define LED2 22
+#define LED_2 22
 #define FAROL 21
-
-//nao deixar o define ser 0
 
 BluetoothSerial SerialBT;
 
 volatile char comando;
 
-Carrinho meuCarrinho(pin1, pin2, pin3, pin4, LED);
+Carrinho meuCarrinho(pin1, pin2, pin3, pin4, LED, LED_2, FAROL);
 
 
 void setup() {
     inicializarSistema();
-    Serial.println("Mudouou x3 adInfinitum final agoraVai Ladinho SuperSsom2 noMore");
-    pinMode(LED2, OUTPUT);
-    pinMode(FAROL, OUTPUT);
+    Serial.println("Sistema inicializado");
 }
 
 void Task1code(void * pvParameters) {
     Serial.println(xPortGetCoreID());
 
-    for (;;) {
+    while(1) {
         if (SerialBT.available()) {
             if (xSemaphoreTake(xMutex, (TickType_t) 10) == pdTRUE) {
                 comando = SerialBT.read();
@@ -45,76 +41,66 @@ void Task1code(void * pvParameters) {
         if (comando == 'F') { //FRENTE
             meuCarrinho.motor_Esq(1);
             meuCarrinho.motor_Dir(1);
-            Serial.println("Siga em frente");
         } 
 
         else if (comando == 'S') { //PARA
             meuCarrinho.motor_Esq(0);
             meuCarrinho.motor_Dir(0);
-            Serial.println("Pare agora");
         } 
 
         else if (comando == 'B') { //RÉ
             meuCarrinho.motor_Esq(-1);
             meuCarrinho.motor_Dir(-1);
-            Serial.println("dó RÉ mi fá sol lá si");
         } 
 
         else if (comando == 'R' || comando == 'E') { //FRETE DIREITA
             meuCarrinho.motor_Esq(0);
             meuCarrinho.motor_Dir(1);
-            Serial.println("Bolsanaro é norte é nordeste");
         } 
 
         else if (comando == 'Q' || comando == 'L') { //FRENTE ESQUERDA
             meuCarrinho.motor_Esq(1);
             meuCarrinho.motor_Dir(0);
-            Serial.println("ole ole ola LULA LULA");
         } 
 
         else if (comando == 'C') { //RÉ DIREITA
             meuCarrinho.motor_Esq(0);
             meuCarrinho.motor_Dir(-1);
-            Serial.println("direitorges");
         } 
 
         else if (comando == 'Z') {//RÉ ESQUERDA
             meuCarrinho.motor_Esq(-1);
             meuCarrinho.motor_Dir(0);
-            Serial.println("esquerdorges");
         }  
 
         else if (comando == 'N') {
-          Serial.println("Ninho, Jun");
           digitalWrite(FAROL, HIGH);
         }
 
         else if (comando == 'n') {
-          Serial.println("Ninho, Jun");
           digitalWrite(FAROL, LOW);
         }
 
-        vTaskDelay(100);
-        
+        vTaskDelay(100);      
     }
 }
 
 void Task2code(void * pvParameters) {
     Serial.println(xPortGetCoreID());
 
-    for (;;) {
+    while(1) {
         if (xSemaphoreTake(xMutex, (TickType_t) 10) == pdTRUE) {
             if (sinal_entre_nucleos) { 
-                //FORMA QUE VAI FICAR LIGADO ou PISCAR
                 digitalWrite(LED, HIGH);
-                digitalWrite(LED2, LOW);
+                digitalWrite(LED_2, LOW);
                 delay(200);
                 digitalWrite(LED, LOW);
-                digitalWrite(LED2, HIGH);
+                digitalWrite(LED_2, HIGH);
                 delay(200);
-            } else {
+            } 
+            else {
                 digitalWrite(LED, LOW);
-                digitalWrite(LED2, LOW);
+                digitalWrite(LED_2, LOW);
                 delay(50);
             }
             xSemaphoreGive(xMutex);
